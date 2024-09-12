db7bc81d9843940af37b8d6e80ed7c525542315b
Beatriz862 <130605338+Beatriz862@users.noreply.github.com>
2024-09-11 22:34:16 -0300

    Add files via upload

---
+++ conversao-temperatura/src/main/java/com/seugrupo/conversaotemperatura/ConversaoTemperaturaApplication.java
@@ -0,0 +1,13 @@
+package com.seugrupo.conversaotemperatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversaoTemperaturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConversaoTemperaturaApplication.class, args);
+    }
+}
+
---
+++ conversao-temperatura/src/test/java/com/seugrupo/conversaotemperatura/ConversaoTemperaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seugrupo.conversaotemperatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversaoTemperaturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
